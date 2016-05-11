55d05bcb8a356c42143e8b922a4a7de13ce7aba4
axelbrethes <axelbrethes@gmail.com>
2016-05-11 16:47:26 +0200

    update administration bundle

--- src/AdminBundle/Controller/AdminController.php
+++ src/AdminBundle/Controller/AdminController.php
@@ -11,7 +11,7 @@ class AdminController extends Controller
 {
     /**
      * @Route("/", name="admin_homepage")
-     * @Template("AdminBundle::base.html.twig")
+     * @Template("AdminBundle::pages/homepage.html.twig")
      */
     public function indexAction(Request $request)
     {
