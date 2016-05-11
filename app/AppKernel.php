25a5b0e9f8c360e9f44f0c4271415d3df711fcb2
axelbrethes <axelbrethes@gmail.com>
2016-05-11 14:10:43 +0200

    add Admin Bundle and review of USER configuration

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
         	//new FOS\FacebookBundle\FOSFacebookBundle(),
             new AppBundle\AppBundle(),
             new UserBundle\UserBundle(),
+            new AdminBundle\AdminBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
---
+++ src/AdminBundle/Controller/AdminController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AdminBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+
+class AdminController extends Controller
+{
+    /**
+     * @Route("/", name="admin_homepage")
+     * @Template("AdminBundle::base.html.twig")
+     */
+    public function indexAction(Request $request)
+    {
+        return array();
+    }
+}
