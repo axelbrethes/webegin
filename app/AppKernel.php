66c46c85310fa95d1effbda0a232a462079ede6f
Axel Brethes <axelbrethes@gmail.com>
2016-05-12 03:54:14 +0200

    Configure HWIOAuthBundle with facebook module

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
         	new Symfony\Bundle\AsseticBundle\AsseticBundle(),
         	new FOS\UserBundle\FOSUserBundle(),
+        	new HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
             new AppBundle\AppBundle(),
             new UserBundle\UserBundle(),
             new AdminBundle\AdminBundle(),
--- src/UserBundle/Entity/User.php
+++ src/UserBundle/Entity/User.php
@@ -12,15 +12,71 @@ use Doctrine\ORM\Mapping as ORM;
 class User extends BaseUser
 {
 	/**
-	 * @ORM\Id
-	 * @ORM\Column(type="integer")
-	 * @ORM\GeneratedValue(strategy="AUTO")
-	 */
-	protected $id;
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+    
+    /** @ORM\Column(name="facebook_id", type="string", length=255, nullable=true) */
+    
+    protected $facebook_id;
+    
+    /** @ORM\Column(name="facebook_access_token", type="string", length=255, nullable=true) */
+    
+    protected $facebook_access_token;
 	
 	public function __construct()
 	{
 		parent::__construct();
 		// your own logic
 	}
+
+    /**
+     * Set facebookId
+     *
+     * @param string $facebookId
+     *
+     * @return User
+     */
+    public function setFacebookId($facebookId)
+    {
+        $this->facebook_id = $facebookId;
+
+        return $this;
+    }
+
+    /**
+     * Get facebookId
+     *
+     * @return string
+     */
+    public function getFacebookId()
+    {
+        return $this->facebook_id;
+    }
+
+    /**
+     * Set facebookAccessToken
+     *
+     * @param string $facebookAccessToken
+     *
+     * @return User
+     */
+    public function setFacebookAccessToken($facebookAccessToken)
+    {
+        $this->facebook_access_token = $facebookAccessToken;
+
+        return $this;
+    }
+
+    /**
+     * Get facebookAccessToken
+     *
+     * @return string
+     */
+    public function getFacebookAccessToken()
+    {
+        return $this->facebook_access_token;
+    }
 }
